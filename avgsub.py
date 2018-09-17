4e58d7392848f17ef11b8cdca2decfc2637787c9
rakeshbishnoi <43162634+rakeshbishnoi@users.noreply.github.com>
2018-09-17 09:33:36 +0530

    Add files via upload

---
+++ avgsub.py
@@ -0,0 +1,5 @@
+#write program to take avg number from user
+a=int(input("enter a value of a:"))
+b=int(input("enter a value of b:"))
+avg=a-b/2
+print(avg)
---
+++ vote.py
@@ -0,0 +1,4 @@
+print("#program to find whether person is eligiable to vote or not")
+print("eligiable to vote above 18 years")
+a=int(input("yes if eligiable:"))
+b=int(input(""))
