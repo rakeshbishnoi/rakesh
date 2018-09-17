72e945e4e96bb7ad3cc794bc8c59c4235a84fc51
rakeshbishnoi <43162634+rakeshbishnoi@users.noreply.github.com>
2018-09-17 22:40:51 +0530

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,4 +1,9 @@
-print("#program to find whether person is eligiable to vote or not")
-print("eligiable to vote above 18 years")
-a=int(input("yes if eligiable:"))
-b=int(input(""))
+#program to find whether person is eligiable to vote or not
+#input a from keyboard 
+a=int(input("eenter the age of a:"))
+#print value of a 
+print ("a:,a")
+if (a>=18):
+  print(a,"is eligiable to vote") 
+  else:
+    print (a,"is not eligiable to vote")
