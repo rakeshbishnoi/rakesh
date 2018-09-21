c62787a7e3e249247f0ef65c668b53c0d662539b
rakeshbishnoi <43162634+rakeshbishnoi@users.noreply.github.com>
2018-09-21 10:00:18 +0530

    Delete vote.py

--- vote.py
+++
@@ -1,9 +0,0 @@
-#program to find whether person is eligiable to vote or not
-#input a from keyboard 
-a=int(input("eenter the age of a:"))
-#print value of a 
-print ("a:,a")
-if (a>=18):
-  print(a,"is eligiable to vote") 
-  else:
-    print (a,"is not eligiable to vote")
