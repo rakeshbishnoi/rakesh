47c23aa5bb05229b7ac83c71b63b49cd02471293
rakeshbishnoi <43162634+rakeshbishnoi@users.noreply.github.com>
2018-09-21 10:28:46 +0530

    Delete printname.py

--- printname.py
+++
@@ -1,7 +0,0 @@
-#a program for printing my name
-print ("rathi")
-#write a program to take two numbers from the user
-a=int(input("enter the value of a"))
-b=int(input("enter the value of b"))
-add=a+b
-print(add)
