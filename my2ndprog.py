94524df92cd49230000ae339eb5a357567de9efc
rakeshbishnoi <43162634+rakeshbishnoi@users.noreply.github.com>
2018-09-24 09:29:24 +0530

    Delete my2ndprog.py

--- my2ndprog.py
+++
@@ -1,7 +0,0 @@
-#prog to find the greatest number
-a=float(input("enter the first number"))
-b=float(input("enter the second number"))
-if(a>b):
-	print(a," is greater")
-else:
-	print(b," is greater")
